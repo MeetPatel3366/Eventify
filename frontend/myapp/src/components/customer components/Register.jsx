47b195142640de5e45114b59e26a870c63079842
Meet Patel <meet3366patel@gmail.com>
2025-12-10 11:01:05 +0530

    refactor registration component

--- frontend/myapp/src/components/customer components/Register.jsx
+++ frontend/myapp/src/components/customer components/Register.jsx
@@ -1,47 +1,37 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../../styles/Register.css"; // Custom CSS for styling
+import axios from "axios";
 
 const Register = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [role, setRole] = useState("customer"); // Default role
-  const [error, setError] = useState("");
-  const [success, setSuccess] = useState("");
+  const [role, setRole] = useState("customer");
+  const [msg, setMsg] = useState("")
   const navigate = useNavigate();
 
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
-
     if (!email || !password) {
-      setError("Please fill in all fields.");
+      setMsg("Please fill in all fields.");
       return;
     }
 
-    // Get existing users from local storage
-    const existingUsers = JSON.parse(localStorage.getItem("users")) || [];
+    const res = await axios.post("http://localhost:4000/api/auth/register", { email, password, role })
+    setMsg(res.data.message);
 
-    // Check if the email is already registered
-    const userExists = existingUsers.some((user) => user.email === email);
-    if (userExists) {
-      setError("Email is already registered.");
-      return;
+    if (res.data.success) {
+      setTimeout(() => {
+        navigate("/")
+      }, 1500)
     }
-
-    // Save new user to local storage
-    const newUser = { email, password, role };
-    localStorage.setItem("users", JSON.stringify([...existingUsers, newUser]));
-
-    setSuccess("Registration successful! Redirecting to login...");
-    setTimeout(() => navigate("/login"), 2000);
   };
 
   return (
     <div className="register-container">
       <div className="register-form">
         <h2>Eventify Registration</h2>
-        {error && <p className="error-message">{error}</p>}
-        {success && <p className="success-message">{success}</p>}
+        {msg && <p>{msg}</p>}
         <form onSubmit={onSubmit}>
           <div className="form-group">
             <label>Email:</label>
@@ -65,7 +55,7 @@ const Register = () => {
             <label>Role:</label>
             <select value={role} onChange={(e) => setRole(e.target.value)}>
               <option value="admin">Admin</option>
-              <option value="employee">Employee</option>
+              <option value="eventorganizer">Event Organizer</option>
               <option value="customer">Customer</option>
             </select>
           </div>
