ba6e388156a4e8dadd2f03e368f2c5f3dd5ca591
Meet Patel <meet3366patel@gmail.com>
2025-12-10 11:01:26 +0530

    refactor logout component

--- frontend/myapp/src/components/customer components/Logout.jsx
+++ frontend/myapp/src/components/customer components/Logout.jsx
@@ -1,17 +1,11 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Logout = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    // Clear any user authentication data (localStorage, sessionStorage, etc.)
-    localStorage.removeItem("authToken"); // Example if using localStorage
-    sessionStorage.clear(); // Example if using sessionStorage
-
-    // Add any additional logout logic if needed (like API call to invalidate token)
-
-    // Navigate to the initial page (home page or login page)
+    localStorage.removeItem("token"); // Example if using localStorage
     navigate("/");
   }, [navigate]);
 
