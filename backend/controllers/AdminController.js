335e3107ba1fe57a1f11567cc0ba633d1a107eba
Meet Patel <meet3366patel@gmail.com>
2025-12-18 11:23:02 +0530

    feat: implement approveOrganizer controller & route

--- backend/controllers/AdminController.js
+++ backend/controllers/AdminController.js
@@ -19,4 +19,39 @@ const getPendingOrganizers = async (req, res) => {
   }
 };
 
-module.exports = { getPendingOrganizers };
+const approveOrganizer = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const organizer = await User.findById(id);
+
+    if (!organizer) {
+      return res.status(404).json({
+        success: false,
+        message: "organizer not found",
+      });
+    }
+
+    if (organizer.organizerStatus !== "pending") {
+      return res.status(400).json({
+        success: false,
+        message: "Organizer already processed",
+      });
+    }
+
+    organizer.organizerStatus = "approved";
+    await organizer.save();
+
+    return res.status(200).json({
+      success: true,
+      message: "organizer approved successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+module.exports = { getPendingOrganizers, approveOrganizer };
--- backend/routes/AdminRoutes.js
+++ backend/routes/AdminRoutes.js
@@ -1,8 +1,13 @@
 const express = require("express");
-const { getPendingOrganizers } = require("../controllers/AdminController");
+const {
+  getPendingOrganizers,
+  approveOrganizer,
+} = require("../controllers/AdminController");
 const { isLoggedIn, isAdmin } = require("../middleware/authMiddleware");
 const router = express.Router();
 
 router.get("/pending-organizers", isLoggedIn, isAdmin, getPendingOrganizers);
+
+router.get("/approve-organizer", isLoggedIn, isAdmin, approveOrganizer);
 
 module.exports = router;
