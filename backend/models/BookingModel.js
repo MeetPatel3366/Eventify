f4e2a83768c865270ae3eec00ccee7263eb9416b
MeetPatel3366 <meet3366patel@gmail.com>
2026-01-02 17:59:43 +0530

    feat: add Booking model with schema for event bookings

---
+++ backend/models/BookingModel.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const BookingSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    eventId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Event",
+      required: true,
+    },
+    quntity: {
+      type: Number,
+      required: true,
+      min: 1,
+    },
+    totalAmount: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "confirmed", "cancelled"],
+      default: "confirmed",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Booking", BookingSchema);
