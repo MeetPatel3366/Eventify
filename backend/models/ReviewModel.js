e375c6699972f1b76fe37f58aea993623a367c4c
MeetPatel3366 <meet3366patel@gmail.com>
2026-01-12 11:32:42 +0530

    feat: create review model

---
+++ backend/models/ReviewModel.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const reviewSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    eventId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Event",
+      required: true,
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: true,
+    },
+    review: {
+      type: String,
+      maxlength: 500,
+    },
+  },
+  { timestamps: true }
+);
+
+// prevent duplicate reviews
+reviewSchema.index({ userId: 1, eventId: 1 }, { unique: true });
+
+module.exports = mongoose.model("Review", reviewSchema);
